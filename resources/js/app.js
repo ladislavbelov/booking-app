07b3b8143a5639fb6a03446d75ebd886b076e1c5
ladislavbelov <klgbelov@gmail.com>
2025-11-21 16:51:06 +0300

    Setup Vue 3 and mount SPA shell on welcome page

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,6 @@
 import './bootstrap';
+
+import { createApp } from 'vue';
+import HelloBooking from './components/HelloBooking.vue';
+
+createApp(HelloBooking).mount('#app');
---
+++ resources/js/components/HelloBooking.vue
@@ -0,0 +1,11 @@
+<script setup>
+const title = 'Booking app';
+const subtitle = 'Первый Vue-компонент внутри Laravel';
+</script>
+
+<template>
+    <div>
+        <h1>{{ title }}</h1>
+        <p>{{ subtitle }}</p>
+    </div>
+</template>
